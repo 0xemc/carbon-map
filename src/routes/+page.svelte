ea0da0026458a53dd7e60e04827b10f96288f445
emc <0xemc@protonmail.com>
2023-07-21 16:40:28 +1000

    init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+	import { onMount } from 'svelte';
+
+	mapboxgl.accessToken =
+		'pk.eyJ1IjoibWljY29sbGlucyIsImEiOiJja3NuMnRrNzkxY2V2MndwYzBueGlpM3d3In0.cMsNf8d1KlEQnvXRVvG3ig';
+
+	onMount(() => {
+		new mapboxgl.Map({
+			container: 'map', // container ID
+			// Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+			style: 'mapbox://styles/mapbox/light-v11', // style URL
+			center: [-68.137343, 45.137451], // starting position
+			zoom: 5 // starting zoom
+		});
+	});
+</script>
+
+<div id="map" />
+
+<style>
+	#map {
+		height: 100vh;
+	}
+</style>

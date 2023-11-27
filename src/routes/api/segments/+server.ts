01522a7ef2a35f2ccb4a843b6c06fff353619839
emc <0xemc@protonmail.com>
2023-11-27 05:56:16 +0100

    timer

--- src/routes/api/segments/+server.ts
+++ src/routes/api/segments/+server.ts
@@ -14,7 +14,9 @@ const supabase = createClient<Database>(url, key, {
 export const GET: RequestHandler = async () => {
 	console.debug('Fetch on / route');
 	try {
+		console.time('supabase-query');
 		const { data, error } = await supabase.from('segments').select('*');
+		console.timeEnd('supabase-query');
 		console.debug('Fetch on / route 2');
 		if (error) throw error;
 		console.debug('Fetch on / route 3');

1acd17a79a3261279e4fe8323f71205f9a9fdd42
emc <0xemc@protonmail.com>
2023-12-03 06:20:17 +0100

    More logging

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,13 +1,21 @@
 import type { Tables } from '$lib/types/database.types.js';
 
 export async function load(event) {
-	const res = await event.fetch('/api/segments');
-	if (res.ok) {
-		const segments: Tables<'segments'>[] = await res.json();
-		return { segments };
+	let res;
+	try {
+		console.debug('load start');
+		console.time('load');
+		res = await event.fetch('/api/segments');
+		console.debug('load end');
+		console.timeEnd('load');
+		if (res.ok) {
+			const segments: Tables<'segments'>[] = await res.json();
+			return { segments };
+		}
+	} catch (e) {
+		console.error(e);
 	}
-
 	return {
-		status: res.status
+		status: res?.status
 	};
 }
